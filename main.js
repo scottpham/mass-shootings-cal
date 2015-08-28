3f7e964bdbfedfc8502968b6885f8ef4093c6f94
scottpham <scottgpham@gmail.com>
2015-08-27 18:16:51 -0700

    all months, plus css effects

--- main.js
+++ main.js
@@ -1,43 +1,38 @@
-mayDays = [];
-
-function getDays(year, month){
-  //get last day
-  var result = new Date(year, month+1, 0);
-  //returns number of last day of the previous month
-  return result.getDate();
-}
-
-
-
-//write array of days
-//javascript days are 1 based
-  for(i = 1; i < getDays(2015, 4); i++){
-    //get datetime for day
-    var day = new Date(2015, 4, i);
-    var element = {"day": day};
-    mayDays.push(element);
-  }
-
-function iterDays(year, monthNum, monthName){
-  for(i = 1; i < getDays(year, monthNum); i++){
-      //day of month
-      var day = new Date(year, monthNum, i);
-      //element of an array
-      var element = {"day": day};
-      monthName.push(element);
-  }
-}
-
 var y2015 = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-iterDays(2015, 7, (August = new Array));
-iterDays(2015, 4, (May = new Array));
+//year 2015 is an empty object
+var year2015 = [];
 
-//var year = ["September":8, "October":9, "November":10];
+//run this anon func for each element in y2015
+y2015.map(function(month){
+   var monthNum = y2015.indexOf(month);
+    //get last day of the month in temp var
+   var temp = new Date(2015, monthNum+1, 0);
+    //get number of days in month
+   var numDays = temp.getDate();
 
-//for(i=0)
-
-
+//store month string in a var
+  var str = String(month);
+   //create blank array to store days
+   var mo = new Array();
+  console.log(str);
+    //get array of days - i is num of day
+   for(i = 1; i <= numDays; i++){
+    //get js day object for day i
+    var day = new Date(2015, monthNum, i);
+    //store day in a {"day": <object>} format
+    var element = {"day": day};
+    //add element to month array
+    mo.push(element);
+   }//end for
+  //console.log(year2015);
+    var mObject = {"month": {
+                    "name": str,
+                    "days": mo }
+                  };
+    //at each index, a month object
+   year2015[monthNum] = mObject;
+});
 
 var ractive = new Ractive({
     // The `el` option can be a node, an ID, or a CSS selector.
@@ -47,32 +42,43 @@ var ractive = new Ractive({
     decorators: {},
     // JSON CAN GO HERE
     data: {
-      may: May,
-      August: August,
+      y15: year2015,
       year: 2015,
       daySize: 40,
       xscale: function(day){
-        var dayofmonth = day.getDate();
         //return a num 1 to 7
-        var dayofweek = dayofmonth % 7;
-        return (dayofweek * this.get('daySize') + 10);
+        var dayofWeek = day.getDay();
+        return (dayofWeek * this.get('daySize') + 10);
       },
       yscale: function(day){
+        //find the 'offset': the day of week of
+        //the first day of this month
+        //get month from day
+        var monthNum = day.getMonth();
+        //get first day of this month
+        var firstDay = new Date(2015, monthNum, 1);
+        var offset = firstDay.getDay();
+
+        //get day of month
         var dayofmonth = day.getDate();
-        var week = Math.floor(dayofmonth/7);
-        return week * this.get('daySize') + 25;
+        //get num week
+        var week = Math.ceil( (dayofmonth + offset) / 7 );
+        return week * this.get('daySize');
       }
     },
     // these properties depend on other properties
     computed: {
+        //can compute daysize here
     },
     oninit: function() {
-        console.log(this.get('may'));
-    }
+        console.log(this.get('y15'));
+    },
+    onrender: function(){
+  }
 });
 
 //proxy events
 //http://docs.ractivejs.org/latest/proxy-events
-ractive.on('select', function(event) {
-
+ractive.on('render', function(event) {
+  console.log(this.get('year'));
 });
